2900215d030c280921f610fe399ce69a20a73df2
tomkulish <thomas.kulish@gmail.com>
2015-12-30 20:33:38 -0500

    Initial commit

---
+++ stream/src/test/scala/com/tomkulish/AppTest.scala
@@ -0,0 +1,17 @@
+package com.tomkulish
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
